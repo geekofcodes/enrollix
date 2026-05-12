0ddbbfcafe4a02ce800dd688d75a6286e59398fa
G Madhu Sudan Reddy <madhureddy2019cse@gmail.com>
2026-05-12 16:32:03 +0530

    feat(api): attach JWT token to requests using axios interceptor

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,6 +4,18 @@ export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
+// REQUEST INTERCEPTOR
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+// RESPONSE INTERCEPTOR
 api.interceptors.response.use(
   (response) => response,
   (error) => {
